c87374032978304d1afa91b7cffcfd384bfc1ac1
Alesc0 <pedroalexandreftw@gmail.com>
2023-01-30 10:45:58 +0000

    feat: autoupdate total order price

--- shop/static/js/cartActions.js
+++ shop/static/js/cartActions.js
@@ -1,3 +1,4 @@
+//add item to cart
 $('button[class^="addCart"]').click(function (e) {
   e.preventDefault();
   $.ajax({
@@ -11,6 +12,7 @@ $('button[class^="addCart"]').click(function (e) {
   });
 });
 
+//remove item from cart
 $('button[name^="removeFromCart"]').click(function (e) {
   e.preventDefault();
   $.ajax({
@@ -22,11 +24,13 @@ $('button[name^="removeFromCart"]').click(function (e) {
       if ($("div[class^='product']").length == 0) {
         $("#cart").html("Your cart is empty");
         $("#checkoutBtn").remove();
+        updateTotal();
       }
     }
   });
 });
 
+//change item quantity
 $('input[name^="quantity"]').change(function (e) {
   e.preventDefault();
   $.ajax({
@@ -36,6 +40,24 @@ $('input[name^="quantity"]').change(function (e) {
   }).done((response) => {
     if (response == "success") {
       $(this).val(this.value);
+      updateTotal();
     }
   });
 });
+
+$(document).ready(function () {
+  updateTotal();
+});
+
+function updateTotal() {
+  let total = 0;
+  $("*[class=product]").each(function () {
+    total += parseFloat(
+      $(this).children("input[name='quantity']").val() *
+        parseFloat(
+          $(this).children("p[id='product-price']").text().slice(0, -1)
+        )
+    );
+  });
+  $("#totalprice").text("Total price: " + total.toFixed(2) + "€");
+}
